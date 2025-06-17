2dc8d03edf14b5c99aee08ca27b76b3ffc27907a
devsafix <devsafix@gmail.com>
2025-06-17 23:10:39 +0600

    ZOD Validation setup done

--- src/app/controllers/users.controller.ts
+++ src/app/controllers/users.controller.ts
@@ -1,11 +1,46 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { User } from "../models/users.model";
+import { z } from "zod";
 
 export const usersRouter = express.Router();
 
+const createUserZodSchema = z.object({
+  firstName: z
+    .string()
+    .min(5, "First name must be at least 5 characters")
+    .max(10, "First name must be at most 10 characters")
+    .trim(),
+  lastName: z
+    .string()
+    .min(5, "Last name must be at least 5 characters")
+    .max(10, "Last name must be at most 10 characters")
+    .trim(),
+  email: z
+    .string()
+    .email("Invalid email address")
+    .regex(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Invalid email format"
+    )
+    .trim()
+    .toLowerCase(),
+  role: z
+    .enum(["USER", "ADMIN"])
+    .default("USER")
+    .transform((val) => val.toUpperCase()), // since mongoose stores it uppercase
+  age: z
+    .number({
+      required_error: "Age is required",
+      invalid_type_error: "Age must be a number",
+    })
+    .min(18, "Age must be at least 18")
+    .max(60, "Age must be at most 60"),
+  password: z.string().min(1, "Password is required").trim(),
+});
+
 usersRouter.post("/create-user", async (req: Request, res: Response) => {
-  const userData = req.body;
+  const userData = await createUserZodSchema.parseAsync(req.body);
   try {
     const user = await User.create(userData);
     res.status(201).json({
